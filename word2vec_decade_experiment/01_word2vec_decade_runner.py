2aaed8b54739502f63954cb1a827f49e4d0b9e69
David Nicholson <dnicholson329@gmail.com>
2021-04-27 12:12:41 -0400

    updted notebooks for submission

--- word2vec_decade_experiment/01_word2vec_decade_runner.py
+++ word2vec_decade_experiment/01_word2vec_decade_runner.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
 # ---
 # jupyter:
 #   jupytext:
+#     cell_metadata_filter: -all
 #     formats: ipynb,py
 #     text_representation:
 #       extension: .py
@@ -16,6 +19,9 @@
 # # Run Word2vec on abstracts for each Decade
 
 # +
+# %load_ext autoreload
+# %autoreload 2
+
 from pathlib import Path
 
 from gensim.models import Word2Vec, KeyedVectors
@@ -26,90 +32,71 @@ import random
 import re
 import tqdm
 
-from biovectors_modules.word2vec_run_helper import (
-    get_gene_disease_pairs,
-    SentencesIterator,
-    similarity_score,
-)
-
-
+from biovectors_modules.word2vec_run_helper import PubMedSentencesIterator, chunks
 # -
 
-def get_year(pub_date):
-    """
-    Extracts four-digit year from publication's date string.
-    """
-    return re.search(r"\d\d\d\d", pub_date).group()
+# # Set up the Data
 
-
-# ## Set up the Data
-
-pairs = get_gene_disease_pairs(
-    Path("output/hetnet_gene_disease_pairs.tsv"), Path("output/DO-slim-to-mesh.tsv")
+pubtator_central_metadata_df = pd.read_csv(
+    "../exploratory_data_analysis/output/pmc_metadata.tsv.xz", sep="\t"
 )
-pairs[0:10]
+print(pubtator_central_metadata_df.shape)
+pubtator_central_metadata_df.head()
 
-dates_df = pd.read_csv(
-    Path("../pubmed_timestamp_experiment/output") / Path("pmid_to_pub_date.tsv.xz"),
-    compression="xz",
-    sep="\t",
+
+pubtator_abstract_batch = list(Path("../pubtator_abstracts").rglob("*.gz"))
+print(len(pubtator_abstract_batch))
+
+year_mapper_df = (
+    pubtator_central_metadata_df.query("published_year != 0")
+    .groupby(["published_year", "batch_folder"])
+    .agg({"batch_file": "unique"})
+    .reset_index()
 )
-print(dates_df.shape)
-dates_df.head()
+year_mapper_df.head()
 
-dates_df = (
-    dates_df.dropna(subset=["pub_date"])
-    .assign(pub_date=lambda x: x.pub_date.apply(get_year).astype(int))
-    .query("~pub_date.isnull()")
-)
-print(dates_df.shape)
-dates_df.head()
+batch_file_year_dict = {}
+for idx, row in tqdm.tqdm(year_mapper_df.iterrows()):
+    if row["published_year"] not in batch_file_year_dict:
+        batch_file_year_dict[row["published_year"]] = {}
 
-# ## Run the Models
-
-# iterate through abstracts from 1971-2020 by decade
-years = [1971, 1981, 1991, 2001, 2011]
-Path("output/decades").mkdir(exists_ok=True)
-Path("output/decades/models").mkdir(exists_ok=True)
-
-for year in years:
-    print(f"----- {str(year)} - {str(year+9)} -----")
-
-    pmids = set(
-        dates_df.loc[dates_df["pub_date"].between(year, year + 9), "pmid"].tolist()
+    batch_file_year_dict[row["published_year"]][row["batch_folder"]] = list(
+        row["batch_file"]
     )
 
-    if len(pmids) > 0:
-        print(f"{len(pmids)} PMIDs from this time period")
-        sentences = SentencesIterator(
-            Path("inputs/bioconcepts2pubtatorcentral.gz"), pmids
-        )
+# # Run the Models
 
-        # check if more than one abstract exists for year
-        count = 0
-        for sentence in tqdm.tqdm(sentences):
-            if count > 0:
-                break
-            count += 1
+# Generate path to save word2vec models
+Path("output/models").mkdir(exist_ok=True)
+years = (
+    pubtator_central_metadata_df.query("published_year>=2000")
+    .published_year.unique()
+    .tolist()
+)
 
-        if count > 0:
-            print("At least one abstract available")
-            print("Creating word2vec model")
-            model = Word2Vec(sentences, size=500, window=5, min_count=1, workers=4)
-            model.save(
-                Path(f"output/decades/models/word2vec_{str(year)}-{str(year+9)}.model")
-            )
-            print("Saved word2vec model")
 
-            (
-                similarity_score(model, pairs, years).to_csv(
-                    f"outputs/decades/similarity_scores_{str(year)}-{str(year+9)}.tsv",
-                    sep="\t",
-                    index=False,
-                )
-            )
+# iterate through all abstracts through all years
+random.seed(100)
+for idx, year in enumerate(chunks(sorted(years), 1)):
 
-        else:
-            print("No abstracts available")
-    else:
-        print("No PMIDs from this time period")
+    if Path(f"output/models/word2vec_{str(year)}.model").exists():
+        continue
+
+    random.shuffle(pubtator_abstract_batch)
+
+    doc_iterator = PubMedSentencesIterator(
+        pubtator_abstract_batch,
+        batch_mapper=batch_file_year_dict[year[0]],
+        year_filter=year,
+        jobs=40,
+    )
+
+    model = Word2Vec(size=300, window=5, min_count=1, workers=8, seed=100)
+    model.build_vocab(doc_iterator)
+
+    model.train(doc_iterator, epochs=5, total_examples=model.corpus_count)
+
+    model.save(str(Path(f"output/models/word2vec_{str(year[0])}.model")))
+
+    print(f"Saved {str(year[0])} word2vec model")
+
--- word2vec_decade_experiment/02_word2vec_decade_analysis.py
+++ word2vec_decade_experiment/02_word2vec_decade_analysis.py
@@ -16,58 +16,147 @@
 # # Analyze Word2Vec by Decades Run
 
 # +
-from pathlib import Path
+# %load_ext autoreload
+# %autoreload 2
 
+from pathlib import Path
+import pickle
+import itertools
+
+from gensim.models import Word2Vec
 import numpy as np
 import pandas as pd
+from scipy.spatial.distance import cdist, pdist, squareform
+from scipy.linalg import orthogonal_procrustes
+import tqdm
 import plotnine as p9
-from sklearn.metrics import (
-    auc,
-    roc_curve,
-    precision_recall_curve,
+
+from biovectors_modules.word2vec_analysis_helper import (
+    get_global_distance,
+    get_local_distance,
+    window,
 )
 # -
 
-# ## Load and Parse the performance data
+# ## Load Models and Parse Performance
 
-decade_df_dict = {}
-years = [1971, 1981, 1991, 2001, 2011]
+# Align word2vec Models since cutoff year
+year_cutoff = 2000
+aligned_model_file_path = "output/aligned_word_vectors.pkl"
+token_occurence_file = "output/earliest_token_occurence.tsv"
+year_distance_folder = "year_distances"
 
-# iterate through results from 1971-2020 by decade
-for year in years:
-    scores_df = pd.read_csv(
-        Path(f"outputs/decades/similarity_scores_{str(year)}-{str(year+9)}.tsv"),
-        sep="\t",
-    )
-
-    fp, tp, _ = roc_curve(
-        scores_df["class"].values.tolist(), scores_df.score.values.tolist()
-    )
-
-    precision, recall, _ = precision_recall_curve(
-        scores_df["class"].values.tolist(), scores_df.score.values.tolist()
-    )
-
-    decade_df_dict[f"{str(year)}-{str(year+9)}"] = pd.DataFrame(
-        {"fp": fp, "tp": tp, "precision": precision, "recall": recall}
-    )
-
-# ## Plot Performance
-
-year = 2001
-
-g = (
-    p9.ggplot(decade_df_dict[f"{str(year)}-{str(year+9)}"], p9.aes(x="fp", y="tp"))
-    + p9.geom_point()
-    + p9.geom_line(p9.aes(x=[0, 1], y=[0, 1]), linetype="dashed")
+# Skip 2021 as that model is too small to analyze
+# Try again December 2021
+word_models = filter(
+    lambda x: int(x.stem.split("_")[1]) >= year_cutoff
+    and int(x.stem.split("_")[1]) != 2021,
+    list(Path("output/models").rglob("*model")),
 )
-print(g)
+word_models = sorted(word_models, key=lambda x: int(x.stem.split("_")[1]), reverse=True)
+print(word_models)
 
-g = (
-    p9.ggplot(
-        decade_df_dict[f"{str(year)}-{str(year+9)}"],
-        p9.aes(x="recall", y="precision"),
+if not Path(token_occurence_file).exists():
+    word_model_dict = dict()
+    earliest_token_occurence = dict()
+    for model in reversed(word_models):
+        year = int(model.stem.split("_")[1])
+        model = Word2Vec.load(str(model))
+        for token in model.wv.vocab.keys():
+            if token not in earliest_token_occurence:
+                earliest_token_occurence[token] = f"{year}"
+            else:
+                earliest_token_occurence[token] += f"|{year}"
+        (
+            pd.DataFrame(
+                list(earliest_token_occurence.items()),
+                columns=["token", "year_occured"],
+            ).to_csv(token_occurence_file, sep="\t", index=False)
+        )
+
+if not Path(aligned_model_file_path).exists():
+    word_model_dict = dict()
+    shared_tokens = set()
+    for model in word_models:
+        year = int(model.stem.split("_")[1])
+        word_model_dict[year] = Word2Vec.load(str(model))
+        if len(shared_tokens) == 0:
+            shared_tokens = set(word_model_dict[year].wv.vocab.keys())
+        else:
+            shared_tokens &= set(word_model_dict[year].wv.vocab.keys())
+
+    shared_tokens = sorted(list(shared_tokens))
+
+# ## Calculate Global and Local Distances Between Time Periods
+
+# ### Align Models via Orthogonal Procrustes
+
+if not Path(aligned_model_file_path).exists():
+    years_analyzed = sorted(list(word_model_dict.keys()), reverse=True)
+    latest_year = years_analyzed[0]
+    aligned_models = {}
+
+    # Years must be in sorted descending order
+    for year_pair in window(years_analyzed):
+
+        year_pair_label = "_".join(map(str, year_pair[::-1]))
+
+        if year_pair[0] == latest_year:
+            aligned_models[str(year_pair[0])] = word_model_dict[year_pair[0]].wv[
+                shared_tokens
+            ]
+
+        else:
+
+            aligned_models[str(year_pair[0])] = word_model_dict[year_pair[0]].wv[
+                shared_tokens
+            ]
+
+            translation_matrix, scale = orthogonal_procrustes(
+                word_model_dict[year_pair[0]].wv[shared_tokens],
+                word_model_dict[latest_year].wv[shared_tokens],
+            )
+
+        translation_matrix, scale = orthogonal_procrustes(
+            word_model_dict[year_pair[1]].wv[shared_tokens],
+            word_model_dict[latest_year].wv[shared_tokens],
+        )
+
+        aligned_models[str(year_pair[1])] = (
+            word_model_dict[year_pair[1]].wv[shared_tokens] @ translation_matrix
+        )
+
+    aligned_models["shared_tokens"] = shared_tokens
+
+if not Path(aligned_model_file_path).exists():
+    pickle.dump(aligned_models, open(aligned_model_file_path, "wb"))
+
+# ### Calculate the Global and Local Distances between Words
+
+aligned_models = pickle.load(open(aligned_model_file_path, "rb"))
+years_analyzed = sorted(list(aligned_models.keys()), reverse=True)[1:]
+n_neighbors = 25
+
+shared_tokens = sorted(aligned_models["shared_tokens"])
+for key in tqdm.tqdm(window(years_analyzed)):
+
+    global_distance = get_global_distance(
+        aligned_models[key[0]], aligned_models[key[1]], aligned_models["shared_tokens"]
     )
-    + p9.geom_point()
-)
-print(g)
+
+    local_distance = get_local_distance(
+        aligned_models[key[0]],
+        aligned_models[key[1]],
+        aligned_models["shared_tokens"],
+        neighbors=n_neighbors,
+    )
+
+    label = "_".join(reversed(key))
+
+    (
+        global_distance.merge(local_distance)
+        .assign(shift=lambda x: x.global_dist.values - x.local_dist.values)
+        .to_csv(
+            f"output/{year_distance_folder}/{label}_dist.tsv", sep="\t", index=False
+        )
+    )
